b3f5d8329e48aab6f9215e8a151f959638bbbea4
Ariel Stoler <amstoler@gmail.com>
2018-01-26 14:33:34 -0500

    Loop "mostly" finished. Pending additon of parama comp to assignment.

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,4 +1,4 @@
-package com.example.demo;//package com.example.fizzbuzz;
+package com.example.demo;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,31 +14,39 @@ public class MainController {
 
         //Initializing variables
 
-        String display = "";
+        String display = " ";
         double valueCheck;
+        int userValueResult;
 
 
-
-        Scanner keyboard = new Scanner(System.in); //Invoking scanner function for future user input
+        Scanner keyboard = new Scanner(System.in); //Invoking scanner function called keyboard for future user input
         System.out.println("Enter a positive integer greater than 0");
         int userValue = keyboard.nextInt(); // Stores number for problem start
 
         do {
             valueCheck = userValue % 2;
-            if (valueCheck ==0) {
-                display = (userValue/2);
-            }
-            if (valueCheck !=0) {
-                display = ((userValue*3) + 1);
 
-            } while (userValue !=1);
+            if (valueCheck == 0) {
+                userValueResult = (userValue / 2);
+                display = userValue + " " + display + " " + userValueResult + " ";
+                userValue = userValueResult;
+
+            }
+
+            if (valueCheck != 0) {
+                userValueResult = ((userValue * 3) + 1);
+                display = userValue + " " + display + " " + userValueResult + " ";
+                userValue = userValueResult;
+            }
+
+
+            }while (userValue != 1);
+
+        //display = "Your webpage prints" + display + userValue;
+        System.out.println(display);
+        return display;
+
         }
 
-
-        
-
-
-        return display ; //"numberFizzBuzz" + " " + "numberFizz" + " " + "numberBuzz";
     }
-}
 
