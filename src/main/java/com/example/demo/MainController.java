792144a844e1cfbe89c793decfbba101fa6f85c7
Ariel Stoler <amstoler@gmail.com>
2018-01-27 11:03:34 -0500

    Formatted final text displayed on screen with result. Cleaned out unused code and lines used for debugging to console.

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -13,16 +13,17 @@ public class MainController {
     public String MagicNumber(@RequestParam("userValue") int userValue) {
 
 
-        //Initializing variables
-
         String display = " ";
         double valueCheck;
         int userValueResult;
+        String message_First = " ";
 
-
-       //  Scanner keyboard = new Scanner(System.in); //Invoking scanner function called keyboard for future user input
-       //  System.out.println("Enter a positive integer greater than 0");
+        // Next 3 lines used in Part 1 of Magic Number pattern. User is entering to console.
+        //  Scanner keyboard = new Scanner(System.in); //Invoking scanner function called keyboard for future user input
+        //  System.out.println("Enter a positive integer greater than 0");
         // int userValue = keyboard.nextInt(); // Stores number for problem start
+
+        message_First = "Your web page prints:" + "<br>" + "Starting Number:" + userValue + "<br>" + "<br>";
 
 
         do {
@@ -30,49 +31,27 @@ public class MainController {
 
             if (valueCheck == 0) {
                 userValueResult = (userValue / 2);
-                //System.out.println("A value is " + userValue);
-                //display =  userValue + " " + display + " " + userValueResult + " ";
                 display += userValue + "<br>";
-                System.out.println("Even number display: " + display);
                 userValue = userValueResult;
-                //System.out.println("A value is" + userValue);
 
-            }
-
-            else if (valueCheck != 0) {
+            } else if (valueCheck != 0) {
                 userValueResult = ((userValue * 3) + 1);
-                //System.out.println("A value is " + userValue);
-                //display = userValue + " " + display + " " + userValueResult + " ";
                 display += userValue + "<br>";
-                System.out.println("Odd number display: " + display);
                 userValue = userValueResult;
-                //System.out.println("B value is" + userValue);
+
 
             }
 
 
-            }while (userValue != 1);
+        } while (userValue != 1);
 
         display += userValue + "<br>";
-        //display = "Your webpage prints" + display + userValue;
-        System.out.println(display);
-        return display;
 
-        }
-
-
-//        @RequestMapping
-//        public int acceptNumber(@RequestParam("number") int number )
-//        {
-//           /* String userNumber ="";
-//            Scanner keyboard = new Scanner (System.in);
-//            userNumber = keyboard.nextLine();
-//           */
-//
-//           return number;
-//
-//
-//        }
+        return message_First + display;
 
     }
+}
+
+
+
 
